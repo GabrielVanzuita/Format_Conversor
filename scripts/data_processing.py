49faf55afabf450249c1d2064e77de5a2b9ce7b5
Gabriel_Vanzuita <gbrvbusiness@gmail.com>
2024-03-29 23:34:16 -0300

    First project importation

---
+++ scripts/data_processing.py
@@ -0,0 +1,76 @@
+import csv 
+import json
+
+class Data: 
+    
+    def __init__(self, data):
+        self.data = data
+        self.name_columns = self._get_columns()
+        self.asset_length = self._size_linhas()
+
+    def _size_linhas(self):
+        return len(self.data)
+
+    def _get_columns(self):
+        return list(self.data[0].keys())
+
+    @staticmethod
+    def _reading_csv(path):
+        with open(path, 'r') as file:
+            data_csv = []
+            spamreader = csv.DictReader(file, delimiter=',')
+            for row in spamreader:
+                data_csv.append(row)
+        return data_csv        
+        
+    @staticmethod
+    def _reading_json(path):
+        with open(path, 'r') as file:
+            data_json = json.load(file)
+        return data_json 
+
+    @classmethod
+    def reading_data(cls, path, type):
+        data = []
+
+        if type == 'csv':
+            data = cls._reading_csv(path)
+        elif type == 'json':
+            data = cls._reading_json(path)
+
+        return data
+    
+    def rename_columns(self, key_mapping):
+        new_data_csv = []
+        for old_dict in self.data:
+            temporary_dict = {}
+            for key, object in old_dict.items():
+                temporary_dict[key_mapping.get(key)] =  object
+            new_data_csv.append(temporary_dict)
+    
+        self.data = new_data_csv
+    
+    @staticmethod
+    def join(dataA, dataB):
+        combined_list = []
+        combined_list.extend(dataA)
+        combined_list.extend(dataB)
+        return combined_list
+
+    def _transforming_table(self):
+        dados_combinados = [self.name_columns]
+
+        for row in self.data:
+            linha = []
+            for coluna in self.name_columns:
+                linha.append(row.get(coluna, 'Indisponível'))
+            dados_combinados.append(linha)
+
+        return dados_combinados
+    
+    def saving_data(self, path):
+        dados_combinados = self._transforming_table()
+
+        with open(path, 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(dados_combinados)
---
+++ scripts/lab_area.py
@@ -0,0 +1,41 @@
+####################################################
+                    #PREPARATION
+####################################################
+import csv 
+import json
+from data_processing import Data
+
+file_path_B = 'Documents/pipeline_dados/data_raw/data_B.csv'
+file_path_A = 'Documents/pipeline_dados/data_raw/data_A.json'
+path_processed_data = 'Documents/pipeline_dados/data_processed/combined_data.csv'
+
+key_mapping = {
+    'Nome do Item': 'Nome do Produto',
+    'Classificação do Produto': 'Categoria do Produto',
+    'Valor em Reais (R$)': 'Preço do Produto (R$)',
+    'Quantidade em Estoque': 'Quantidade em Estoque',
+    'Nome da Loja': 'Filial',
+    'Data da Venda': 'Data da Venda'
+}
+
+####################################################
+                    #READING
+####################################################
+
+data_A = Data.reading_data(file_path_A, 'json')  
+data_B = Data.reading_data(file_path_B, 'csv')  
+
+####################################################
+                    #FUSION
+####################################################
+
+merged_data = Data.join(data_A, data_B)
+#print(merged_data)
+
+####################################################
+                    #SAVING
+####################################################
+
+
+merged_data_instance = Data(merged_data)
+merged_data_instance.saving_data(path_processed_data)
